a44afd018e3836280271321ada0158afbedb5fd5
Eshwarivb <eshwarivb6@gmail.com>
2026-04-05 20:55:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from env import GuessEnv
+
+app = FastAPI()
+
+env = None
+
+@app.post("/reset")
+def reset():
+    global env
+    env = GuessEnv(1, 100)
+    obs = env.reset()
+    return {"observation": obs}
+
+
+@app.post("/step")
+def step(action: int):
+    global env
+    result = env.step(action)
+    return result
